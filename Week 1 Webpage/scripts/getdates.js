8771cffa8b4719053a02ff1db0ea5ea02613d6ea
Garikai88 <gmangobe@byupathway.edu>
2025-11-03 11:44:57 +0200

    Webpage week 1

---
+++ Week 1 Webpage/scripts/getdates.js
@@ -0,0 +1,6 @@
+// Display current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+// Display last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
